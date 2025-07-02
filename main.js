6e7ca402609ab9018dddad2176febeb6fb349931
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2025-07-02 22:38:53 +0530

    consoles

--- main.js
+++ main.js
@@ -15,3 +15,9 @@ const twoSum = function (nums, target) {
 };
 
 console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSum([3, 3], 6)); // Output: [0, 1]
+console.log(twoSum([1, 2, 3, 4, 5], 10)); // Output: []
+console.log(twoSum([1, 2, 3, 4, 5], 9)); // Output: [3, 4]
+console.log(twoSum([1, 2, 3, 4, 5], 8)); // Output: [2, 4]
+console.log(twoSum([1, 2, 3, 4, 5], 7)); // Output: [1, 4]
